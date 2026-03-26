5842593da8e332918ee4392a57a49a811f06e430
RHELENPLUS <raulx7v7@gmail.com>
2026-03-26 15:42:46 -0400

    feat(ui): make logout button functional and display dynamic user data
    - Link the logout button in profile bubble to AuthController::logout
    - Display the actual full name and role of the logged-in user from the session variables

--- app/Views/partials/profile_bubble.php
+++ app/Views/partials/profile_bubble.php
@@ -3,6 +3,8 @@
  * Partial: Burbuja de Perfil de Usuario (User Menu)
  * Incluido en: partials/navbar.php
  */
+$userName = $_SESSION['user_name'] ?? 'Invitado';
+$userRol = $_SESSION['user_rol'] ?? 'S/R';
 ?>
 <!--inicio::Menú de Usuario-->
 <li class="nav-item dropdown user-menu">
@@ -12,7 +14,7 @@
       class="user-image rounded-circle shadow"
       alt="User Image"
     />
-    <span class="d-none d-md-inline">Ven-911</span>
+    <span class="d-none d-md-inline"><?= htmlspecialchars($userName) ?></span>
   </a>
   <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-end">
     <li class="user-header text-bg-primary">
@@ -22,13 +24,13 @@
         alt="User Image"
       />
       <p>
-        Ven911
-        <small>Miembro desde 2024</small>
+        <?= htmlspecialchars($userName) ?>
+        <small><?= htmlspecialchars($userRol) ?></small>
       </p>
     </li>
     <li class="user-footer">
       <a href="#" class="btn btn-default btn-flat">Perfil</a>
-      <a href="#" class="btn btn-default btn-flat float-end">Salir</a>
+      <a href="index.php?url=auth/logout" class="btn btn-default btn-flat float-end">Salir</a>
     </li>
   </ul>
 </li>
